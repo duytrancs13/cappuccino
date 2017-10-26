e0a49819daf0b85810ad31372ec3b9c700364602
tkduy3295 <tkduy3295@gmail.com>
2017-10-26 18:04:53 +0700

    Xử lý validate Email và Password.

--- app/src/main/java/info/devexchanges/navvp/LoginActivity.java
+++ app/src/main/java/info/devexchanges/navvp/LoginActivity.java
@@ -1,6 +1,8 @@
 package info.devexchanges.navvp;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.ActionBar;
@@ -9,8 +11,11 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
@@ -18,8 +23,11 @@ import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 public class LoginActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
-
     private TextView tvForgotPw;
+
+
+    private EditText editEmailLogin, editPwLogin;
+    private Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,6 +55,88 @@ public class LoginActivity extends AppCompatActivity {
         tvForgotPw = (TextView) findViewById(R.id.tvForgotPw);
         tvForgotPw.setText(Html.fromHtml("<u>Quên mật khẩu</u>"));
 
+
+        editEmailLogin = (EditText) findViewById(R.id.editEmailLogin);
+        editPwLogin = (EditText) findViewById(R.id.editPwLogin);
+
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login();
+            }
+        });
+
+    }
+
+    private void login() {
+        if(!validate()){
+            onLoginFailed();
+            return ;
+        }
+
+        btnLogin.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this,
+                R.style.AppTheme_Dark_Dialog);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Đang kết nối...");
+        progressDialog.show();
+
+        String email = editEmailLogin.getText().toString();
+        String password = editPwLogin.getText().toString();
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        // On complete call either onLoginSuccess or onLoginFailed
+                        onLoginSuccess();
+                        // onLoginFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+
+    }
+
+    private void onLoginSuccess() {
+        btnLogin.setEnabled(true);
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+
+    private void onLoginFailed() {
+        toast("Login failed");
+        btnLogin.setEnabled(true);
+    }
+
+    private boolean validate(){
+        boolean valid = true;
+
+        String email = editEmailLogin.getText().toString();
+        String password = editPwLogin.getText().toString();
+
+        if(email.isEmpty()){
+            editEmailLogin.setError("Vui lòng nhập email");
+            valid = false;
+        }else if(!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editEmailLogin.setError("Nhập không đúng định dạng email!!!");
+            valid = false;
+        }else{
+            editEmailLogin.setError(null);
+        }
+
+        if(password.isEmpty() ){
+            editPwLogin.setError("Vui lòng nhập mật khẩu");
+            valid = false;
+        }else if(password.length() < 6){
+            editPwLogin.setError("Mật khẩu nhiều hơn 5 kí tự!!!");
+            valid = false;
+        }else{
+            editPwLogin.setError(null);
+        }
+
+        return valid;
     }
 
     protected void attachBaseContext(Context newBase) {
@@ -60,6 +150,12 @@ public class LoginActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    public void toast(String message){
+        Toast.makeText(getBaseContext(),message,Toast.LENGTH_SHORT).show();
+    }
+
+
     /*@Override
     public void onBackPressed() {
         finish();
